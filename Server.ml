809d1647952f5d01864f2a1348b9070a8904e081
jose nogueira <ze@thatportugueseguy.com>
2019-07-04 19:27:35 +0100

    Add ocamlformat config and format code

--- Server.ml
+++ Server.ml
@@ -1,7 +1,6 @@
 open Base
 open Lwt.Infix
 module Arg = Caml.Arg
-
 open Httpaf_lwt_unix
 open RequestHandler
 open ErrorHandler
@@ -9,23 +8,17 @@ open ErrorHandler
 let main port =
   let listen_address = Unix.(ADDR_INET (inet_addr_loopback, port)) in
   Lwt.async (fun () ->
-      Lwt_io.establish_server_with_client_socket
-        listen_address
+      Lwt_io.establish_server_with_client_socket listen_address
         (Server.create_connection_handler ~request_handler ~error_handler)
       >|= fun _server ->
       Stdio.printf "Notabot Started.\n\n";
       Stdio.printf "Listening on port %i.\n%!" port);
   let forever, _ = Lwt.wait () in
   Lwt_main.run forever
-;;
 
 let () =
   let port = ref 8080 in
   Arg.parse
-    ["-p", Arg.Set_int port, " Listening port number (8080 by default)"]
-    ignore
-    "Notabot, the notifications bot. Your options:";
+    [ "-p", Arg.Set_int port, " Listening port number (8080 by default)" ]
+    ignore "Notabot, the notifications bot. Your options:";
   main !port
-;;
-
-
