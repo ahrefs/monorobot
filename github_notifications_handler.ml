f0acc053d03e4fe6889b62f9e774310c23313d4c
jose nogueira <ze@thatportugueseguy.com>
2019-07-22 21:12:14 +0100

    Fix request signature validation

--- github_notifications_handler.ml
+++ github_notifications_handler.ml
@@ -1,7 +1,6 @@
 open Httpaf
 open Base
 open Github_events_notifications_t
-open Configuration.Env
 
 type event_name =
   | Push_event
@@ -20,16 +19,20 @@ let parse_notification_payload body event_name =
   | Ok CI_run_event -> Ok (CI_run (Github_events_notifications_j.ci_build_notification_of_string body))
   | Error error_message -> Error error_message
 
-let is_valid_sig_and_user_agent req_sig user_agent =
-  String.equal req_sig (Lazy.force github_sha1_signature) && String.equal user_agent (Lazy.force github_user_agent)
+let calculate_request_hash key body = Cstruct.of_string body |> Nocrypto.Hash.SHA1.hmac ~key |> Cstruct.to_string
 
-let validate_request_event_headers headers =
+let is_valid_request_signature headers_sig body =
+  let github_webhook_secret = Lazy.force Configuration.Env.github_webhook_secret_token in
+  let (`Hex request_hash) = calculate_request_hash (Cstruct.of_string github_webhook_secret) body |> Hex.of_string in
+  String.equal headers_sig (Printf.sprintf "sha1=%s" request_hash)
+
+let validate_request_event_headers headers body =
   let get_headers = Headers.get headers in
-  match get_headers "X-Hub-Signature", get_headers "User-Agent", get_headers "X-GitHub-Event" with
-  | Some req_sig, Some ua, Some event_type when is_valid_sig_and_user_agent req_sig ua ->
+  match get_headers "X-Hub-Signature", get_headers "X-GitHub-Event" with
+  | Some req_sig, Some event_type when is_valid_request_signature req_sig body ->
     ( match event_type with
-      | "push" -> Ok Push_event
-      | "pull_request" -> Ok Pull_request_event
-      | "check_suite" -> Ok CI_run_event
-      | _ -> Error (Printf.sprintf "Unsupported github event: %s" event_type) )
-  | _, _, _ -> Error "Headers validation failed"
+    | "push" -> Ok Push_event
+    | "pull_request" -> Ok Pull_request_event
+    | "check_suite" -> Ok CI_run_event
+    | _ -> Error (Printf.sprintf "Unsupported github event: %s" event_type) )
+  | _, _ -> Error "Headers validation failed"
