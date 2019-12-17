018349516e484cfffd0da3e26b58e6d779c08faa
ip <igor@ahrefs.com>
2019-12-17 13:29:03 +0200

    separate event processing

---
+++ src/action.ml
@@ -0,0 +1,8 @@
+open Slack
+
+let generate_notification req =
+  match req with
+  | Github.Push n -> Ok (generate_push_notification n)
+  | Pull_request n -> Ok (generate_pull_request_notification n)
+  | CI_run n when is_success_or_failed n.state -> Ok (generate_ci_run_notification n)
+  | _ -> Error ()
