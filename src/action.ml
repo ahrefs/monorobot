634cf50612072794bcbc1b4f0b60500b556f4806
ip <igor@ahrefs.com>
2019-12-17 13:57:13 +0200

    send PR notification only once per PR (when opened)

--- src/action.ml
+++ src/action.ml
@@ -3,6 +3,6 @@ open Slack
 let generate_notification req =
   match req with
   | Github.Push n -> Ok (generate_push_notification n)
-  | Pull_request n -> Ok (generate_pull_request_notification n)
+  | Pull_request n when n.action = Opened -> Ok (generate_pull_request_notification n)
   | CI_run n when is_success_or_failed n.state -> Ok (generate_ci_run_notification n)
   | _ -> Error ()
