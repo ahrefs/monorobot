f00d494410e3b0492716296b99e44de3edaf0b5b
ip <igor@ahrefs.com>
2019-12-18 11:15:04 +0200

    print routing at startup

--- src/action.ml
+++ src/action.ml
@@ -1,8 +1,8 @@
 open Base
 open Slack
+open Notabot_t
 
 let touching rule files =
-  let open Notabot_t in
   files |> List.exists ~f:begin fun file ->
     String.is_prefix file ~prefix:rule.prefix && not (List.exists ~f:(fun ignore -> String.is_prefix file ~prefix:ignore) rule.ignore)
   end
@@ -19,9 +19,18 @@ let partition_push rules n =
   end
 
 let generate_notifications cfg req =
-  let open Notabot_t in
   match req with
   | Github.Push n -> partition_push cfg.rules n |> List.map ~f:(fun (rule,n) -> rule.webhook, generate_push_notification n)
 (*   | Pull_request n when Poly.(n.action = Opened) -> [slack_notabot, generate_pull_request_notification n] *)
 (*   | CI_run n when Poly.(n.state <> Success) -> [slack_notabot, generate_ci_run_notification n] *)
   | _ -> []
+
+let print_routing rules =
+  rules |> List.iter ~f:begin fun rule ->
+    Stdio.printf "  match %s*" rule.prefix;
+    begin match rule.ignore with
+    | [] -> ()
+    | l -> Stdio.printf " and not %s" (String.concat ~sep:" or " @@ List.map ~f:(fun s -> s ^ "*") l)
+    end;
+    Stdio.printf " -> #%s\n%!" rule.webhook.channel;
+  end
