6229a7497e70bec0300e8dded977ae977a6337f9
ip <igor@ahrefs.com>
2019-12-18 11:15:04 +0200

    print routing at startup

--- src/notabot.ml
+++ src/notabot.ml
@@ -13,15 +13,17 @@ let error_handler (_ : Unix.sockaddr) ?request:_ error start_response =
   Body.close_writer response_body
 
 let main port =
+  Stdio.print_endline "Notabot starting";
   let cfg = Notabot_j.config_of_string @@ Stdio.In_channel.read_all "notabot.json" in
+  Stdio.print_endline "Using routing:";
+  Action.print_routing cfg.rules;
   let listen_address = Unix.(ADDR_INET (inet_addr_loopback, port)) in
   let request_handler = Request_handler.request_handler cfg in
   Lwt.async (fun () ->
       Lwt_io.establish_server_with_client_socket listen_address
         (Server.create_connection_handler ~request_handler ~error_handler)
       >|= fun _server ->
-      Stdio.printf "Notabot Started.\n\n";
-      Stdio.printf "Listening on port %i.\n%!" port);
+      Stdio.printf "Listening on port %i\n%!" port);
   let forever, _ = Lwt.wait () in
   Lwt_main.run forever
 
