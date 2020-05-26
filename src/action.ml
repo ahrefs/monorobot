780dca5fb81509d197334151679303501132767d
Xinyu Luo <xinyu.luo@ahrefs.com>
2020-05-26 15:02:31 +0800

    allowed diversified messge for different action types in pull request; add test cases for pr review comments

--- src/action.ml
+++ src/action.ml
@@ -51,29 +51,43 @@ let partition_push cfg n =
        | [] -> None
        | l -> Some (rule, { n with commits = l }))
 
-let partition_pr cfg pr =
-  let labels = pr.labels in
+let partition_pr cfg n =
+  let labels = n.pull_request.labels in
   match labels with
-  | [] -> Option.value_map cfg.pr_rules.default ~default:[] ~f:(fun webhook -> [ webhook ])
+  | [] -> Option.value_map cfg.pr_rules.default ~default:[] ~f:(fun webhook -> [ webhook, n ])
   | labels ->
     cfg.pr_rules.rules
     |> List.filter_map ~f:(fun rule ->
          match exist_label rule labels with
          | false -> None
-         | true -> Some rule.webhook)
+         | true ->
+         match n.action with
+         | Github_t.Opened | Closed | Reopened -> Some (rule.webhook, n)
+         | _ -> None)
 
-let partition_pr_event cfg n = partition_pr cfg n.pull_request
-
-let partition_pr_review_comment cfg (n : pr_review_comment_notification) = partition_pr cfg n.pull_request
+let partition_pr_review_comment cfg (n : pr_review_comment_notification) =
+  let labels = n.pull_request.labels in
+  match labels with
+  | [] -> Option.value_map cfg.pr_rules.default ~default:[] ~f:(fun webhook -> [ webhook, n ])
+  | labels ->
+    cfg.pr_rules.rules
+    |> List.filter_map ~f:(fun rule ->
+         match exist_label rule labels with
+         | false -> None
+         | true ->
+         match n.action with
+         | Github_t.Created -> Some (rule.webhook, n)
+         | _ -> None)
 
 let generate_notifications cfg req =
   match req with
   | Github.Push n ->
     partition_push cfg n |> List.map ~f:(fun ((rule : prefix_rule), n) -> rule.webhook, generate_push_notification n)
   | Github.Pull_request n ->
-    partition_pr_event cfg n |> List.map ~f:(fun webhook -> webhook, generate_pull_request_notification n)
+    partition_pr cfg n |> List.map ~f:(fun (webhook, n) -> webhook, generate_pull_request_notification n)
   | Github.PR_review_comment n ->
-    partition_pr_review_comment cfg n |> List.map ~f:(fun webhook -> webhook, generate_pr_review_comment_notification n)
+    partition_pr_review_comment cfg n
+    |> List.map ~f:(fun (webhook, n) -> webhook, generate_pr_review_comment_notification n)
   (*   | CI_run n when Poly.(n.state <> Success) -> [slack_notabot, generate_ci_run_notification n] *)
   | _ -> []
 
