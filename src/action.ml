3e5709f52e634d784ae42da00f02c601f970c959
Xinyu Luo <xinyu.luo@ahrefs.com>
2020-05-27 19:07:31 +0800

    handled issue events and issue comment events

--- src/action.ml
+++ src/action.ml
@@ -51,34 +51,34 @@ let partition_push cfg n =
        | [] -> None
        | l -> Some (rule, { n with commits = l }))
 
-let partition_pr cfg n =
+let partition_label cfg labels =
+  match labels with
+  | [] -> Option.value_map cfg.label_rules.default ~default:[] ~f:(fun webhook -> [ webhook ])
+  | labels ->
+    cfg.label_rules.rules
+    |> List.filter_map ~f:(fun rule ->
+         match exist_label rule labels with
+         | false -> None
+         | true -> Some rule.webhook)
+
+let partition_pr cfg (n : pr_notification) =
   match n.action with
-  | Opened | Closed | Reopened ->
-    let labels = n.pull_request.labels in
-    ( match labels with
-    | [] -> Option.value_map cfg.pr_rules.default ~default:[] ~f:(fun webhook -> [ webhook, n ])
-    | labels ->
-      cfg.pr_rules.rules
-      |> List.filter_map ~f:(fun rule ->
-           match exist_label rule labels with
-           | false -> None
-           | true -> Some (rule.webhook, n))
-    )
+  | Opened | Closed | Reopened -> partition_label cfg n.pull_request.labels
+  | _ -> []
+
+let partition_issue cfg (n : issue_notification) =
+  match n.action with
+  | Opened | Closed | Reopened -> partition_label cfg n.issue.labels
   | _ -> []
 
 let partition_pr_review_comment cfg (n : pr_review_comment_notification) =
   match n.action with
-  | Created ->
-    let labels = n.pull_request.labels in
-    ( match labels with
-    | [] -> Option.value_map cfg.pr_rules.default ~default:[] ~f:(fun webhook -> [ webhook, n ])
-    | labels ->
-      cfg.pr_rules.rules
-      |> List.filter_map ~f:(fun rule ->
-           match exist_label rule labels with
-           | false -> None
-           | true -> Some (rule.webhook, n))
-    )
+  | Created -> partition_label cfg n.pull_request.labels
+  | _ -> []
+
+let partition_issue_comment cfg (n : issue_comment_notification) =
+  match n.action with
+  | Created -> partition_label cfg n.issue.labels
   | _ -> []
 
 let generate_notifications cfg req =
@@ -86,10 +86,12 @@ let generate_notifications cfg req =
   | Github.Push n ->
     partition_push cfg n |> List.map ~f:(fun ((rule : prefix_rule), n) -> rule.webhook, generate_push_notification n)
   | Github.Pull_request n ->
-    partition_pr cfg n |> List.map ~f:(fun (webhook, n) -> webhook, generate_pull_request_notification n)
+    partition_pr cfg n |> List.map ~f:(fun webhook -> webhook, generate_pull_request_notification n)
   | Github.PR_review_comment n ->
-    partition_pr_review_comment cfg n
-    |> List.map ~f:(fun (webhook, n) -> webhook, generate_pr_review_comment_notification n)
+    partition_pr_review_comment cfg n |> List.map ~f:(fun webhook -> webhook, generate_pr_review_comment_notification n)
+  | Github.Issue n -> partition_issue cfg n |> List.map ~f:(fun webhook -> webhook, generate_issue_notification n)
+  | Github.Issue_comment n ->
+    partition_issue_comment cfg n |> List.map ~f:(fun webhook -> webhook, generate_issue_comment_notification n)
   (*   | CI_run n when Poly.(n.state <> Success) -> [slack_notabot, generate_ci_run_notification n] *)
   | _ -> []
 
