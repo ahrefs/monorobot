21e72972c2ad8c2763021514f7d593339cc7797d
Louis Roché <louis.roche@ahrefs.com>
2020-05-29 19:58:54 +0800

    add tests

---
+++ test/test.ml
@@ -0,0 +1,31 @@
+open Base
+open Lib
+
+let print_notif (webhook, msg) =
+  let json =
+    msg |> Slack_j.string_of_webhook_notification |> Yojson.Basic.from_string |> Yojson.Basic.pretty_to_string
+  in
+  Stdio.printf "will notify #%s\n" webhook.Notabot_t.channel;
+  Stdio.printf "%s" json
+
+let process cfg file =
+  Stdio.printf "file %s\n" file;
+  match Mock.kind file with
+  | None -> ()
+  | Some kind ->
+    let headers = Httpaf.Headers.of_list [ "X-GitHub-Event", kind ] in
+    ( match Github.parse_exn ~secret:None headers (Stdio.In_channel.read_all file) with
+    | exception exn ->
+      Stdio.printf "exception when parsing %s: %s\n" file (Exn.to_string exn);
+      ()
+    | event ->
+      let notifs = Action.generate_notifications cfg event in
+      List.iter notifs ~f:print_notif
+    )
+
+let () =
+  let mock_dir = "../mock_payloads" in
+  let jsons = Caml.Sys.readdir mock_dir in
+  let jsons = Array.map ~f:(fun p -> Caml.Filename.concat mock_dir p) jsons in
+  let cfg = Notabot_j.config_of_string @@ Stdio.In_channel.read_all "notabot.json" in
+  Array.iter ~f:(process cfg) jsons
