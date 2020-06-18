b55611c517c7e0a58e7845d1118d730a91f2e499
Louis Roché <louis.roche@ahrefs.com>
2020-06-18 18:09:25 +0800

    add --github-token option

--- src/notabot.ml
+++ src/notabot.ml
@@ -5,17 +5,17 @@ module Arg = Caml.Arg
 
 let log = Log.from "notabot"
 
-let get_config path =
-  let cfg = Config.load path in
+let get_config path gh_token =
+  let cfg = Config.load ?gh_token path in
   log#info "using prefix routing:";
   Action.print_prefix_routing cfg.prefix_rules.rules;
   log#info "using label routing:";
   Action.print_label_routing cfg.label_rules.rules;
   cfg
 
-let http_server addr port config =
+let http_server addr port config gh_token =
   log#info "notabot starting";
-  let cfg = get_config config in
+  let cfg = get_config config gh_token in
   log#info "signature checking %s" (if Option.is_some cfg.gh_webhook_secret then "enabled" else "disabled");
   Lwt_main.run (Request_handler.start_http_server ~cfg ~addr ~port ())
 
@@ -25,8 +25,8 @@ let send_slack_notification webhook file =
   | exception exn -> log#error ~exn "unable to parse notification"
   | data -> Lwt_main.run (Slack.send_notification webhook data)
 
-let check_common file print config =
-  let cfg = get_config config in
+let check_common file print config gh_token =
+  let cfg = get_config config gh_token in
   match Mock.kind file with
   | None ->
     log#error "aborting because payload %s is not named properly, named should be KIND.NAME_OF_PAYLOAD.json" file;
@@ -61,16 +61,21 @@ let print_json_message (chan, msg) =
   log#info "%s" (Uri.to_string url);
   log#info "%s" json
 
-let check file json config =
+let check file json config gh_config =
   Lwt_main.run
     ( match json with
-    | false -> check_common file print_simplified_message config
-    | true -> check_common file print_json_message config
+    | false -> check_common file print_simplified_message config gh_config
+    | true -> check_common file print_json_message config gh_config
     )
 
 (** {2 cli} *)
 
 open Cmdliner
+
+let github_token =
+  let doc = "github token, will override the one from the json configuration if present" in
+  let env = Arg.env_var "GITHUB_TOKEN" in
+  Arg.(value & opt (some string) None & info [ "github-token" ] ~docv:"GITHUB_TOKEN" ~doc ~env)
 
 let addr =
   let doc = "http listen addr" in
@@ -103,13 +108,13 @@ let json =
 let run =
   let doc = "launch the http server" in
   let info = Term.info "run" ~doc in
-  let term = Term.(const http_server $ addr $ port $ config) in
+  let term = Term.(const http_server $ addr $ port $ config $ github_token) in
   term, info
 
 let check =
   let doc = "read github notification payload from file and show actions to be taken" in
   let info = Term.info "check" ~doc in
-  let term = Term.(const check $ mock_payload $ json $ config) in
+  let term = Term.(const check $ mock_payload $ json $ config $ github_token) in
   term, info
 
 let slack_notif =
