6f6d3c7a7f742e7140d367375adcfdc205bd01e0
Haliq <abdul.haliq@ahrefs.com>
2020-07-09 11:21:36 +0800

    replace use of cfg and separate load functions with context; ctx

--- test/test.ml
+++ test/test.ml
@@ -8,13 +8,7 @@ let print_notif (chan, msg) =
   Stdio.printf "will notify #%s\n" chan;
   Stdio.printf "%s\n" json
 
-let load_initial_state state_dir file =
-  let basename = Caml.Filename.basename file in
-  let state_file = Caml.Filename.concat state_dir basename in
-  Devkit.Log.set_loglevels "error";
-  State.load state_file
-
-let process cfg state_dir file =
+let process state_dir cfg_path secrets_path file =
   Stdio.printf "===== file %s =====\n" file;
   match Mock.kind file with
   | None -> Lwt.return_unit
@@ -25,21 +19,19 @@ let process cfg state_dir file =
       Stdio.printf "exception when parsing %s: %s\n" file (Exn.to_string exn);
       Lwt.return_unit
     | event ->
-      let state = ref (load_initial_state state_dir file) in
-      let load_state () = !state in
-      let update_state s e = state := State.update_state s e in
-      let%lwt notifs = Action.generate_notifications cfg load_state update_state event in
+      Devkit.Log.set_loglevels "error";
+      let state_path = Caml.Filename.concat state_dir @@ Caml.Filename.basename file in
+      let ctx = Context.make ~state_path ~cfg_path ~secrets_path ~disable_write:true () in
+      let%lwt notifs = Action.generate_notifications ctx event in
       List.iter notifs ~f:print_notif;
       Lwt.return_unit
     )
 
 let () =
-  let cfg = Config.load "notabot.json" "secrets.json" in
   let mock_dir = "../mock_payloads" in
-  let state_dir = "../mock_states" in
   let jsons = Caml.Sys.readdir mock_dir in
   let jsons = Array.map ~f:(fun p -> Caml.Filename.concat mock_dir p) jsons in
   Array.sort jsons ~compare:String.compare;
   Lwt_main.run
     (let jsons = Array.to_list jsons in
-     Lwt_list.iter_s (process cfg state_dir) jsons)
+     Lwt_list.iter_s (process "../mock_states" "notabot.json" "secrets.json") jsons)
