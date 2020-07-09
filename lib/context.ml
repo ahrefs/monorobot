e9d0053fe8831320a79648897fecc395a8c2c603
Haliq <abdul.haliq@ahrefs.com>
2020-07-09 10:06:10 +0800

    rename symbol

--- lib/context.ml
+++ lib/context.ml
@@ -3,11 +3,11 @@ type t = {
   mutable cfg : Config.t;
   load_state : unit -> Notabot_t.state;
   update_state : Notabot_t.state -> Github.t -> unit;
-  get_config : unit -> Config.t;
+  load_config : unit -> Config.t;
 }
 
 let make ~state_path ~path ~secrets =
-  let get_config' () = Config.load path secrets in
+  let load_config' () = Config.load path secrets in
   let load_state' () = State.load state_path in
 
   let cfg_setter r x = r.cfg <- x in
@@ -17,10 +17,10 @@ let make ~state_path ~path ~secrets =
     x
   in
 
-  let rec get_config () = set_and_id r cfg_setter @@ get_config' ()
-  and cfg = get_config' ()
+  let rec load_config () = set_and_id r cfg_setter @@ load_config' ()
+  and cfg = load_config' ()
   and load_state () = set_and_id r state_setter @@ load_state' ()
   and update_state state event = State.save state_path @@ set_and_id r state_setter @@ State.update_state state event
   and state = load_state' ()
-  and r = { get_config; cfg; load_state; update_state; state } in
+  and r = { load_config; cfg; load_state; update_state; state } in
   r
