a41dfaf806a382900e1e96886f619b27f3ddb386
Haliq <abdul.haliq@ahrefs.com>
2020-07-09 11:37:52 +0800

    extract initial values out of recursive definition

--- lib/context.ml
+++ lib/context.ml
@@ -10,20 +10,21 @@ let make ~state_path ~cfg_path ~secrets_path ?(disable_write = false) () =
   let load_config' () = Config.load cfg_path secrets_path in
   let load_state' () = State.load state_path in
   let update_state' state event = State.update_state state event in
-
+  (* context values helper functions *)
+  let cfg = load_config' () in
+  let state = load_state' () in
+  (* initial context values *)
   let cfg_setter r x = r.cfg <- x in
   let state_setter r x = r.state <- x in
   let set r setter x =
     setter r x;
     x
   in
-
+  (* mutable field helper functions *)
   let rec load_config () = set r cfg_setter @@ load_config' ()
-  and cfg = load_config' ()
   and load_state () = set r state_setter @@ load_state' ()
   and update_state state event =
     let act = if disable_write then ignore else State.save state_path in
     act @@ set r state_setter @@ update_state' state event
-  and state = load_state' ()
   and r = { load_config; cfg; load_state; update_state; state } in
   r
