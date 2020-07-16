61a62087cefab5c47a22a6a49d9e2126f6c41d2d
Haliq <abdul.haliq@ahrefs.com>
2020-07-16 15:10:35 +0800

    omit unecessary type annotation and reorder definitions in let rec

--- lib/mrkdwn.ml
+++ lib/mrkdwn.ml
@@ -5,7 +5,15 @@ let escape_url_chars str =
   let repl c = String.substr_replace_all ~pattern:(Char.to_string c) ~with_:(Printf.sprintf "\\%c" c) in
   str |> repl '<' |> repl '>' |> repl '|'
 
-let rec transform = function
+let rec transform_list = List.map ~f:transform
+
+and transform_flatten = List.map ~f:transform_list
+
+and surround s t =
+  let t = to_markdown @@ transform_list t in
+  Raw (Printf.sprintf "%s%s%s" s t s)
+
+and transform = function
   | H1 t -> H1 (transform_list t)
   | H2 t -> H2 (transform_list t)
   | H3 t | H4 t | H5 t | H6 t -> Paragraph [ Bold (transform_list t) ]
@@ -27,15 +35,7 @@ let rec transform = function
   | Img (alt, src, title) -> Url (src, [ Text alt ], title)
   | (Text _ | Code _ | Code_block _ | Br | Hr | NL | Ref _ | Img_ref _ | Raw _ | Raw_block _ | X _) as e -> e
 
-and transform_list = List.map ~f:transform
-
-and transform_flatten = List.map ~f:transform_list
-
-and surround s t =
-  let t = to_markdown @@ transform_list t in
-  Raw (Printf.sprintf "%s%s%s" s t s)
-
-let of_doc (t : t) = transform_list t
+let of_doc t = transform_list t
 
 let to_mrkdwn doc = to_markdown @@ of_doc doc
 
