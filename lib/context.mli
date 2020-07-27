374cb44f00f13e0fa43908c7788ac79f1aa2deac
Haliq <liqo12@hotmail.com>
2020-07-27 13:55:21 +0800

    made mli file for context

---
+++ lib/context.mli
@@ -0,0 +1,73 @@
+exception Context_Error of string
+
+type cfg_sources =
+  | Local
+  | Remote
+
+type data = {
+  mutable cfg_path : string;
+  cfg_source : cfg_sources;
+  cfg_action_after_refresh : Config.t -> unit;
+  secrets_path : string;
+  state_path : string;
+  disable_write : bool;
+}
+
+type t = {
+  mutable state : Notabot_t.state;
+  mutable cfg : Config.t;
+  secrets : Notabot_t.secrets;
+  data : data;
+}
+
+type context_thunk = {
+  secrets : Notabot_j.secrets;
+  thunk : ?req:Github.t -> unit -> t;
+  mutable ctx : t option;
+}
+
+val refresh_config : t -> unit
+
+val refresh_and_get_config : t -> Config.t
+
+val change_remote_url : t -> Github.t -> unit
+
+val refresh_state : t -> unit
+
+val refresh_and_get_state : t -> Notabot_t.state
+
+val update_state : t -> Github.t -> unit
+
+val update_and_get_state : t -> Github.t -> Notabot_t.state
+
+val make_with_secrets
+  :  state_path:string ->
+  ?cfg_path:string ->
+  secrets_path:string ->
+  secrets:Notabot_t.secrets ->
+  ?disable_write:bool ->
+  ?cfg_action_after_refresh:(Config.t -> unit) ->
+  ?req:Github.t ->
+  unit ->
+  t
+
+val make
+  :  state_path:string ->
+  ?cfg_path:string ->
+  secrets_path:string ->
+  ?disable_write:bool ->
+  ?cfg_action_after_refresh:(Config.t -> unit) ->
+  ?req:Github.t ->
+  unit ->
+  t
+
+val make_thunk
+  :  state_path:string ->
+  ?cfg_path:string ->
+  secrets_path:string ->
+  ?disable_write:bool ->
+  ?cfg_action_after_refresh:(Config.t -> unit) ->
+  unit ->
+  context_thunk
+
+val resolve_ctx_in_thunk : context_thunk -> Github.t -> t
