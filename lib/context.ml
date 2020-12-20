5618a48c1af94c7db74c9b9ef2ae123ce39b3fc4
Yasunari Watanabe <yasunari.watanabe@ahrefs.com>
2020-12-20 21:52:25 +0800

    edit context structure and define getters for secrets+config Secrets are now in their own record field within the context, so that future secret additions don’t pollute the outer context structure. Getters are also defined to reduce boilerplate exception handling.

--- lib/context.ml
+++ lib/context.ml
@@ -2,13 +2,15 @@ open Base
 open Common
 open Devkit
 
+exception Context_error of string
+
+let context_error msg = raise (Context_error msg)
+
 type t = {
   config_filename : string;
   secrets_filepath : string;
   state_filepath : string option;
-  gh_token : string option;
-  gh_hook_token : string option;
-  slack_hooks : Slack.channel_hook StringMap.t;
+  mutable secrets : Config_t.secrets option;
   mutable config : Config.t option;
   mutable state : State_t.state;
 }
@@ -18,9 +20,7 @@ let default : t =
     config_filename = "notabot.json";
     secrets_filepath = "secrets.json";
     state_filepath = None;
-    gh_token = None;
-    gh_hook_token = None;
-    slack_hooks = StringMap.empty;
+    secrets = None;
     config = None;
     state = State.empty;
   }
@@ -30,7 +30,21 @@ let make ?config_filename ?secrets_filepath ?state_filepath () =
   let secrets_filepath = Option.value secrets_filepath ~default:default.secrets_filepath in
   { default with config_filename; secrets_filepath; state_filepath }
 
-let hook_of_channel ctx channel_name = Map.find ctx.slack_hooks channel_name
+let get_secrets_exn ctx =
+  match ctx.secrets with
+  | None -> context_error "secrets is uninitialized"
+  | Some secrets -> secrets
+
+let get_config_exn ctx =
+  match ctx.config with
+  | None -> context_error "config is uninitialized"
+  | Some config -> config
+
+let hook_of_channel ctx channel_name =
+  let secrets = get_secrets_exn ctx in
+  match List.find secrets.slack_hooks ~f:(fun webhook -> String.equal webhook.channel channel_name) with
+  | Some hook -> Some hook.url
+  | None -> None
 
 let refresh_pipeline_status ctx ~pipeline ~(branches : Github_t.branch list) ~status ~updated_at =
   ctx.state <- State.refresh_pipeline_status ctx.state ~pipeline ~branches ~status ~updated_at
@@ -41,14 +55,8 @@ let refresh_secrets ctx =
   let path = ctx.secrets_filepath in
   match%lwt get_local_file path with
   | Ok res ->
-    let secrets = Config_j.secrets_of_string res in
-    let slack_hooks =
-      List.fold_left secrets.slack_hooks ~init:StringMap.empty ~f:(fun m hook ->
-        Map.set m ~key:hook.channel ~data:hook.url)
-    in
-    let gh_token = secrets.gh_token in
-    let gh_hook_token = secrets.gh_hook_token in
-    Lwt.return @@ Ok { ctx with slack_hooks; gh_token; gh_hook_token }
+    ctx.secrets <- Some (Config_j.secrets_of_string res);
+    Lwt.return @@ Ok ctx
   | Error e ->
     log#error "error while getting local file %s: %s" path e;
     Lwt.return @@ fmt_error "failed to get secrets from file %s" path
