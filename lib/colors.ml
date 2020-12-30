59458ee5b7956db83e31a94f593f85ea955f8d47
Yasunari Watanabe <yasunari.watanabe@ahrefs.com>
2020-12-30 17:52:32 +0800

    add logic for building message body of unfurled commit links
    The new file `slack_message.ml` should only contain generation
    logic for messages that describe the state of some info on GH
    (e.g., a description of a PR), and *not* an action taken on GH
    (e.g, "XYZ opened a PR".
    
    This decoupling is desired because we eventually want to reuse
    code between link unfurl generation and event notification
    generation, and link unfurl only concerns the former type of
    info described above. Currently, it's difficult to do as
    there is tight coupling in `slack.ml`.
    
    Currently we support commit link unfurls. We can later easily
    expand the selection of unfurlable links by adding onto
    `slack_message.ml`.

---
+++ lib/colors.ml
@@ -0,0 +1,13 @@
+(* https://styleguide.github.com/primer/utilities/colors/#background-colors *)
+
+let gray = "#f6f8fa"
+
+let blue = "#0366d6"
+
+let yellow = "#ffd33d"
+
+let red = "#d73a49"
+
+let green = "#28a745"
+
+let purple = "#6f42c1"
