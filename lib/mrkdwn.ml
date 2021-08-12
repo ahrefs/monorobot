f65d55ad4714f5840a444900747837706a71ed5a
Yasunari Watanabe <yasunari.watanabe@ahrefs.com>
2021-08-12 17:11:08 +0800

    mrkdwn: remove old code and promote two tests
    [commit_comment.mrkdwn_comment]
    - add more MD elements to be tested (headings ordered lists, etc.)
    - properly escape characters
    - remove extraneous line after blockquotes and nested lists

--- lib/mrkdwn.ml
+++ lib/mrkdwn.ml
@@ -1,8 +1,6 @@
 open Omd
 open Base
 
-let escape_url_chars = Staged.unstage @@ String.Escaping.escape ~escapeworthy:[ '<'; '>'; '|' ] ~escape_char:'\\'
-
 (* https://api.slack.com/reference/surfaces/formatting#escaping *)
 let escape_mrkdwn =
   String.concat_map ~f:(function
@@ -10,58 +8,6 @@ let escape_mrkdwn =
     | '>' -> "&gt;"
     | '&' -> "&amp;"
     | c -> String.make 1 c)
-
-(** Unescape markdown characters escaped because "any ASCII punctuation
-    character may be backslash-escaped"
-    https://spec.commonmark.org/0.30/#backslash-escapes
-
-    This pertains to '\\', '[', ']', '(', ')', '`', '*' unconditionally,
-    and '.', '-', '+', '!', '<', '>', '#' depending on chars before/after.
-    Argument escapeworthy_map can be left blank because escaped chars are
-    unescaped to themselves. *)
-let unescape_omd = Staged.unstage @@ String.Escaping.unescape_gen_exn ~escapeworthy_map:[] ~escape_char:'\\'
-
-(** Escape the `escape_char` '\\' for use with `unescape_omd` later *)
-let escape_omd = Staged.unstage @@ String.Escaping.escape_gen_exn ~escapeworthy_map:[] ~escape_char:'\\'
-
-let transform_text = escape_mrkdwn
-
-(** `Omd.to_markdown` escapes backslash (and other applicable chars) in
-    `Text` elements but not `Code` elements, so do the same for the latter so
-    that `unescape_omd` can apply uniformly to the whole mrkdwn string later *)
-let transform_code s = escape_omd @@ escape_mrkdwn s
-
-let rec transform_list = List.map ~f:transform
-
-and transform_flatten = List.map ~f:transform_list
-
-and surround s t =
-  let t = to_markdown @@ transform_list t in
-  Raw (Printf.sprintf "%s%s%s" s t s)
-
-(** massage markdown AST so that rendered result looks like slack mrkdwn *)
-and transform = function
-  | H1 t | H2 t | H3 t | H4 t | H5 t | H6 t -> Paragraph (transform_list [ Bold t ])
-  | Paragraph t -> Paragraph (transform_list t)
-  | Emph t -> surround "_" (transform_list t)
-  | Bold t -> surround "*" (transform_list t)
-  | Ul ts -> Ul (transform_flatten ts)
-  | Ol ts -> Ol (transform_flatten ts)
-  | Ulp ts -> Ulp (transform_flatten ts)
-  | Olp ts -> Olp (transform_flatten ts)
-  | Url (href, label, title) ->
-    let label = escape_url_chars @@ to_markdown @@ transform_list label in
-    let title = if String.length title > 0 then Printf.sprintf "%s - " @@ escape_url_chars title else title in
-    Raw (Printf.sprintf "<%s|%s%s>" href title label)
-  | Html _ as e -> Raw (Printf.sprintf "`%s`" @@ to_markdown [ e ])
-  | Html_comment _ -> Br
-  | Html_block _ as e -> Code_block ("", to_markdown [ e ])
-  | Blockquote t -> Blockquote (transform_list t)
-  | Img (alt, src, title) -> transform @@ Url (src, [ Text alt ], title)
-  | Code_block (_, str) -> Code_block ("", transform_code str)
-  | Code (_, str) -> Code ("", transform_code str)
-  | Text s -> Text (transform_text s)
-  | (Br | Hr | NL | Ref _ | Img_ref _ | Raw _ | Raw_block _ | X _) as e -> e
 
 (** Translates omd AST to a Slack mrkdwn string. Code heavily adapted
     from omd 1.3.1 source.
@@ -224,8 +170,7 @@ let rec mrkdwn_of_md md =
       loop list_indent s;
       Buffer.add_char b '>';
       loop list_indent tl
-    | H1 md' | H2 md' | H3 md' | H4 md' | H5 md' | H6 md' ->
-      loop list_indent (Paragraph (transform_list [ Bold md' ]) :: tl)
+    | H1 md' | H2 md' | H3 md' | H4 md' | H5 md' | H6 md' -> loop list_indent (Paragraph [ Bold md' ] :: tl)
     | Br ->
       (* the string "\\n" (backslash-newline) or end of line double-space renders Br *)
       nl b;
@@ -254,4 +199,4 @@ let rec mrkdwn_of_md md =
   end;
   Buffer.contents b
 
-let mrkdwn_of_markdown str = unescape_omd @@ to_markdown @@ transform_list @@ of_string str
+let mrkdwn_of_markdown str = mrkdwn_of_md @@ of_string str
