221b3276feef5601094f1cec4fc06fb9023c58b2
Yasunari Watanabe <yasunari.watanabe@ahrefs.com>
2021-08-18 11:01:05 +0800

    rule: use keyword "any" for global filter override

---
+++ lib/atd_adapters.ml
@@ -0,0 +1,48 @@
+module List_or_default_field = struct
+  open Atdgen_runtime.Json_adapter
+
+  module type Param = sig
+    (** the name of the field *)
+    val field_name : string
+
+    (** string alias a user can use to denote default value *)
+    val default_alias : string
+
+    (** Yojson representation of default value *)
+    val default_value : Yojson.Safe.t
+  end
+
+  module Make (P : Param) : S = struct
+    open P
+
+    let assoc_replace l k v = List.map (fun x -> if fst x = k then k, v else x) l
+
+    let normalize x =
+      match x with
+      | `Assoc fields ->
+        begin
+          match List.assoc field_name fields with
+          | `String s when String.equal s default_alias -> `Assoc (assoc_replace fields field_name default_value)
+          | _ | (exception Not_found) -> x
+        end
+      | malformed -> malformed
+
+    let restore x =
+      match x with
+      | `Assoc fields ->
+        begin
+          match List.assoc field_name fields with
+          | value when Yojson.Safe.equal value default_value -> `Assoc (assoc_replace fields field_name (`String "any"))
+          | _ | (exception Not_found) -> x
+        end
+      | malformed -> malformed
+  end
+end
+
+module Branch_filters_adapter = List_or_default_field.Make (struct
+  let field_name = "branch_filters"
+
+  let default_alias = "any"
+
+  let default_value = `List []
+end)
