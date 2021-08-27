918367c92ff29a9054c53ec816acf6cf411efe4f
ip <igor@ahrefs.com>
2021-08-27 17:55:39 -0400

    shorten unfurl
    author is already in title, no need to spend whole first line on it

--- lib/slack_message.ml
+++ lib/slack_message.ml
@@ -121,25 +121,32 @@ let populate_issue repository (issue : issue) =
 let populate_commit repository (commit : api_commit) =
   let ({ sha; commit; url; author; files; _ } : api_commit) = commit in
   let title =
-    sprintf "`<%s|%s>` *%s - %s*" url (Slack.git_short_sha_hash sha)
+    sprintf "`<%s|%s>` *%s - <%s|%s>*" url (Slack.git_short_sha_hash sha)
       (escape_mrkdwn @@ first_line commit.message)
+      (escape_mrkdwn author.html_url)
       (escape_mrkdwn commit.author.name)
   in
-  let num_change = List.length files in
-  let prefix_path =
-    List.map files ~f:(fun f -> f.filename)
-    |> Common.longest_common_prefix
-    |> String.split ~on:'/'
-    |> List.drop_last_exn
-    |> String.concat ~sep:"/"
+  let changes =
+    match files with
+    | [f] -> sprintf "_modified `%s` (+%d-%d)_" (escape_mrkdwn f.filename) f.additions f.deletions
+    | _ ->
+      let prefix_path =
+        List.map files ~f:(fun f -> f.filename)
+        |> Common.longest_common_prefix
+        |> String.split ~on:'/'
+        |> List.drop_last_exn
+        |> String.concat ~sep:"/"
+      in
+      sprintf "modified %d files in `%s/`" (List.length files) prefix_path
   in
-  let changes = sprintf "%d changed %s in `%s/`" num_change (Slack.pluralize "file" num_change "s") prefix_path in
   let text = sprintf "%s\n%s" title changes in
   let fallback = sprintf "[%s] %s - %s" (Slack.git_short_sha_hash sha) commit.message commit.author.name in
   {
     (base_attachment repository) with
+    (*
     author_name = Some author.login;
     author_link = Some author.html_url;
+    *)
     author_icon = Some author.avatar_url;
     color = Some Colors.gray;
     mrkdwn_in = Some [ "text" ];
