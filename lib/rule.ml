1d31109b20546aa975521ed4609a834dabdb7fcb
ip <igor@ahrefs.com>
2023-02-23 11:02:26 -0500

    minor simplify

--- lib/rule.ml
+++ lib/rule.ml
@@ -135,17 +135,11 @@ module Project_owners = struct
     | _ :: _ ->
       let pr_labels_set = List.map ~f:(fun (l : Github_t.label) -> l.name) pr_labels |> SS.of_list in
       let do_match { label; labels; owners } =
-        let labels =
-          match labels with
-          | [] -> Option.value_map label ~f:(fun l -> [ l ]) ~default:[]
-          | labels -> labels
-        in
         match owners with
         | [] -> false
         | _ :: _ ->
-        match labels with
+        match Option.to_list label @ labels with
         | [] -> false
-        | [ label ] -> SS.mem label pr_labels_set
         | labels -> SS.subset (SS.of_list labels) pr_labels_set
       in
       List.find ~f:do_match rules |> Option.value_map ~default:[] ~f:(fun l -> l.owners)
