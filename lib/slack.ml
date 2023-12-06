ahrefs/monorobot

commit 7ce909b827f10ccbd4056cb7022b215b27bf4c5a

slack & slack_msg: reuse push commit notification truncation for compare

lib/slack.ml
@@ -216,6 +216,25 @@ let generate_issue_comment_notification notification channel =
 
 let git_short_sha_hash hash = String.sub ~pos:0 ~len:8 hash
 
+(** pretty print github commit *)
+let pp_commit ({ url; id; message; author; _ } : commit) =
+  let title = first_line message in
+  sprintf "`<%s|%s>` %s - %s" url (git_short_sha_hash id) title author.name
+
+(** pretty print list with previews of each item per line--will always show at most 7 and drop the rest*)
+let pp_list_with_previews ~pp_item list =
+  let num_items = List.length list in
+  (* truncation point depends on line length, but 7+3 lines seems okay for most cases *)
+  let num_shown = 7 in
+  let dropped = num_items - num_shown in
+  if dropped > 3 then begin
+    let h, list' = List.split_n list (num_shown / 2) in
+    let t = List.drop list' dropped in
+    let num_after_h = num_items - List.length h in
+    List.concat [ List.map ~f:pp_item h; [ sprintf "+%d more...\n" num_after_h ]; List.map ~f:pp_item t ]
+  end
+  else List.map ~f:pp_item list
+
 let generate_push_notification notification channel =
   let { sender; created; deleted; forced; compare; commits; repository; _ } = notification in
   let commits_branch = Github.commits_branch_of_ref notification.ref in
@@ -241,21 +260,7 @@ let generate_push_notification notification channel =
         (if created then "to new branch " else "")
         sender.login
   in
-  let commits =
-    let pp_commit { url; id; message; author; _ } =
-      let title = first_line message in
-      sprintf "`<%s|%s>` %s - %s" url (git_short_sha_hash id) title author.name
-    in
-    (* truncation point depends on line length, but 7+3 lines seems okay for most cases *)
-    let num_shown = 7 in
-    let dropped = num_commits - num_shown in
-    if dropped > 3 then begin
-      let h, commits' = List.split_n commits (num_shown / 2) in
-      let t = List.drop commits' dropped in
-      List.concat [ List.map ~f:pp_commit h; [ sprintf "+%d more..." dropped ]; List.map ~f:pp_commit t ]
-    end
-    else List.map commits ~f:pp_commit
-  in
+  let commits = pp_list_with_previews ~pp_item:pp_commit commits in
   {
     channel;
     text = Some title;

lib/slack_message.ml
@@ -228,8 +228,11 @@ let populate_compare repository (compare : compare) =
   | false ->
     let commits_unfurl = List.map compare.commits ~f:(populate_commit ~include_changes:false repository) in
     let commits_unfurl_text =
-      List.map commits_unfurl ~f:(fun commit_unfurl -> Option.value commit_unfurl.text ~default:"")
+      Slack.pp_list_with_previews
+        ~pp_item:(fun (commit_unfurl : unfurl) -> Option.value commit_unfurl.text ~default:"")
+        commits_unfurl
     in
+
     let commits_unfurl_fallback =
       List.map commits_unfurl ~f:(fun commit_unfurl -> Option.value commit_unfurl.fallback ~default:"")
     in

commit a0ca8425354fd3ba59aa38c2344422d17a9024bf

slack: fix dropped commits count in push message
regressed in 7ce909b827f10ccbd4056cb7022b215b27bf4c5a

lib/slack.ml
@@ -182,8 +182,7 @@ let pp_list_with_previews ~pp_item list =
   if dropped > 3 then begin
     let h, list' = List.split_n list (num_shown / 2) in
     let t = List.drop list' dropped in
-    let num_after_h = num_items - List.length h in
-    List.concat [ List.map ~f:pp_item h; [ sprintf "+%d more...\n" num_after_h ]; List.map ~f:pp_item t ]
+    List.concat [ List.map ~f:pp_item h; [ sprintf "+%d more...\n" dropped ]; List.map ~f:pp_item t ]
   end
   else List.map ~f:pp_item list
 

test/slack_payloads.expected
@@ -411,7 +411,7 @@ will notify #all-push-events
 {
   "channel": "all-push-events",
   "username": "monorepo:develop",
-  "text": "<https://github.com/thatportugueseguy/monorepo/compare/fb245e2a6d52...e2173f38ae43|12 commits> pushed by thatportugueseguy\n`<https://github.com/thatportugueseguy/monorepo/commit/80452f696a8988e7234063d47720a62e902f2afc|80452f69>` Update readme - jose nogueira\n`<https://github.com/thatportugueseguy/monorepo/commit/75170cc6e1b1aa0c603a13b9480ee1e7d1c9b0d7|75170cc6>` Update readme 2 - jose nogueira\n`<https://github.com/thatportugueseguy/monorepo/commit/82e3497b0fb250ef1a19daa0996ae0ad51641bea|82e3497b>` Update readme 3 - jose nogueira\n+9 more...\n\n`<https://github.com/thatportugueseguy/monorepo/commit/d6a17bfc44f7e752e02176ba87ec5a97f5b3316b|d6a17bfc>` Update readme 9 - jose nogueira\n`<https://github.com/thatportugueseguy/monorepo/commit/003927e6c73068f440c28da69386798ece9cb6dc|003927e6>` Update readme 10 - jose nogueira\n`<https://github.com/thatportugueseguy/monorepo/commit/5de643873ff93dc9163c4c63d3fb7c2ee1bb33b4|5de64387>` Update readme 11 - jose nogueira\n`<https://github.com/thatportugueseguy/monorepo/commit/e2173f38ae43865433a182c1fc1b5442d9763b54|e2173f38>` Add TESTING.md - jose nogueira",
+  "text": "<https://github.com/thatportugueseguy/monorepo/compare/fb245e2a6d52...e2173f38ae43|12 commits> pushed by thatportugueseguy\n`<https://github.com/thatportugueseguy/monorepo/commit/80452f696a8988e7234063d47720a62e902f2afc|80452f69>` Update readme - jose nogueira\n`<https://github.com/thatportugueseguy/monorepo/commit/75170cc6e1b1aa0c603a13b9480ee1e7d1c9b0d7|75170cc6>` Update readme 2 - jose nogueira\n`<https://github.com/thatportugueseguy/monorepo/commit/82e3497b0fb250ef1a19daa0996ae0ad51641bea|82e3497b>` Update readme 3 - jose nogueira\n+5 more...\n\n`<https://github.com/thatportugueseguy/monorepo/commit/d6a17bfc44f7e752e02176ba87ec5a97f5b3316b|d6a17bfc>` Update readme 9 - jose nogueira\n`<https://github.com/thatportugueseguy/monorepo/commit/003927e6c73068f440c28da69386798ece9cb6dc|003927e6>` Update readme 10 - jose nogueira\n`<https://github.com/thatportugueseguy/monorepo/commit/5de643873ff93dc9163c4c63d3fb7c2ee1bb33b4|5de64387>` Update readme 11 - jose nogueira\n`<https://github.com/thatportugueseguy/monorepo/commit/e2173f38ae43865433a182c1fc1b5442d9763b54|e2173f38>` Add TESTING.md - jose nogueira",
   "unfurl_links": false
 }
 ===== file ../mock_payloads/push.two_commits_longest_match.json =====