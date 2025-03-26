368dc043c478f7f165db902cccda5f44548c1f7b
Emile Trotignon <emile.trotignon@ahrefs.com>
2025-03-26 14:06:19 +0000

    rename lib to monorobotlib

--- lib/text_cleanup/test/text_cleanup_bin/main.ml
+++ lib/text_cleanup/test/text_cleanup_bin/main.ml
@@ -2,7 +2,8 @@ let () =
   In_channel.with_open_bin Sys.argv.(1) (fun file ->
       let content = In_channel.input_all file in
       let content =
-        if Array.length Sys.argv = 3 && Sys.argv.(2) = "--json" then (Lib.Buildkite_j.job_log_of_string content).content
+        if Array.length Sys.argv = 3 && Sys.argv.(2) = "--json" then
+          (Monorobotlib.Buildkite_j.job_log_of_string content).content
         else content
       in
 
